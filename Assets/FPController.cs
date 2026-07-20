90c85cdb420c562d2fc0cb12fa75a9f57dd31963
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-07-20 21:23:02 +0530

    feat: Adding Charater jump and avoid character double jump through RayCastHit.

--- Assets/FPController.cs
+++ Assets/FPController.cs
@@ -5,10 +5,13 @@ public class FPController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     float speed = 0.1f;
+    Rigidbody rigidbody;
+    CapsuleCollider capsuleCollider;
 
     void Start()
     {
-
+        rigidbody = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
@@ -19,9 +22,29 @@ public class FPController : MonoBehaviour
 
     void FixedUpdate()
     {
+        bool shouldJump = Input.GetKeyDown(KeyCode.Space);
+
+        if (shouldJump && isGrounded())
+        {
+            rigidbody.AddForce(0, 300, 0);
+        }
+
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         transform.position += new Vector3(x * speed, 0, z * speed);
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit hitInformation;
+
+        if (Physics.SphereCast(transform.position, capsuleCollider.radius, Vector3.down, out hitInformation,
+            (capsuleCollider.height / 2f) - capsuleCollider.radius + 0.1f))
+        {
+            return true;
+        }
+        return false;
     }
 }
